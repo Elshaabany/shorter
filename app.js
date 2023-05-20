3c128b5d366da32a8794b155d943cc7817bc56f0
Elshaabany <elshaabany@protonmail.com>
2023-05-20 08:12:03 +0300

    adding URL validation

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express, { json } from 'express';
 import 'express-async-errors';
 import ShortLinkRouter from './routes/shortlink.js';
 import URLRouter from './routes/URL.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(json());
+app.use(cors());
 
 app.use('/shortlink', ShortLinkRouter);
 app.use('/', URLRouter);
--- controller/shortlink.js
+++ controller/shortlink.js
@@ -1,6 +1,19 @@
 import ShortLink from '../model/shortlink.js';
 import { nanoid } from 'nanoid';
 import { host, port } from '../util/config.js';
+import { isUri } from 'valid-url';
+
+function isValidLink(link) {
+	if (
+		isUri(link.web) &&
+		isUri(link.ios.primary) &&
+		isUri(link.ios.fallback) &&
+		isUri(link.android.primary) &&
+		isUri(link.android.fallback)
+	)
+		return true;
+	else return false;
+}
 
 export async function getLink(req, res) {
 	const links = await ShortLink.find();
@@ -30,13 +43,18 @@ export async function postLink(req, res) {
 		web: req.body.web,
 	};
 
+	if (!isValidLink(link)) {
+		res.status(400).send({ message: 'invalid URL format' });
+		return;
+	}
+
 	while (await ShortLink.exists({ slug: link.slug })) {
 		link.slug = nanoid(idLength);
 	}
 
 	const shortLink = await ShortLink.create(link);
 
-	res.json({
+	res.status(201).json({
 		message: 'short link created',
 		shortURL: `http://${host}:${port}/${shortLink.slug}`,
 	});
@@ -49,10 +67,29 @@ export async function putLink(req, res) {
 		return;
 	}
 
-	await shortLink.updateOne({
-		...req.body,
+	const link = {
 		slug: shortLink.slug,
-	});
+		ios: req.body.ios
+			? {
+					primary: req.body.ios.primary,
+					fallback: req.body.ios.fallback,
+			  }
+			: shortLink.ios,
+		android: req.body.android
+			? {
+					primary: req.body.android.primary,
+					fallback: req.body.android.fallback,
+			  }
+			: shortLink.android,
+		web: req.body.web || shortLink.web,
+	};
+
+	if (!isValidLink(link)) {
+		res.status(400).send({ message: 'invalid URL format' });
+		return;
+	}
+
+	await shortLink.updateOne({ ...link });
 
 	res.json({ message: 'link updated' });
 }
